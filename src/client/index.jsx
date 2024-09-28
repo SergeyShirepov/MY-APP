9ded33c4d0c3a2a5d9d3f64488f25240b8d51027
SergeyShirepov <163455887+SergeyShirepov@users.noreply.github.com>
2024-09-28 23:00:58 +0300

    +

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -6,5 +6,5 @@ const container = document.getElementById('react_root');
 if (container) {
     hydrateRoot(container, <App token={window.__token__} />);
 } else {
-    console.error('Root container not found');
+    console.error('Root container not found or is not an Element');
 }
