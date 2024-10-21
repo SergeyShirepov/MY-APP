3abe0dbf0f5a59057fdc31d61bec8f53253d434e
SergeyShirepov <163455887+SergeyShirepov@users.noreply.github.com>
2024-10-21 20:45:53 +0300

    Добавлена передача данных из Reddit через контекст

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import * as styles from './header.css';
 import { SearchBlock } from './SearchBlock';
 import { ThreadTitle } from './ThreadTitle';
 import { SortBlock } from './SortBlock';
+import { tokenContext } from '../context/tokenContext';
 
 interface IHederProps {
   token?: string | undefined
 }
 
-export function Header({ token }: IHederProps) {
+export function Header() {
   return (
 <header className={styles.header}>
-{token ? <SearchBlock token={token} /> : <SearchBlock />}
+  <SearchBlock />  
   <ThreadTitle />
   <SortBlock />
 </header>
