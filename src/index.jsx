0821122d947e46fa6b688a2b6fbc1229c0b3cc57
SergeyShirepov <163455887+SergeyShirepov@users.noreply.github.com>
2024-12-21 23:31:07 +0300

    Небольшие правки карточки

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,21 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "../src/App";
-// import { Header } from "./shared/Header";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-window.addEventListener('load', () => {
-    const rootElement = document.getElementById('react_root');
-    if (rootElement) {
-        const root = createRoot(rootElement);
-        root.render(<App />);
-    } else {
-        console.error('No element with id "react_root" found.');
-    }
-});
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default;
+      root.render(<NextApp />);
+    });
+  }
+} else {
+  console.error('No element with id "root" found.');
+}
 
 
