bb23f85da3c2a3b0ff85f4cd041bed003f19fd8e
SergeyShirepov <shirepov@internet.ru>
2025-01-29 00:28:02 +0300

    Разделение компонента

--- src/shared/CardsList/Card/CommentForm/CommentForm.tsx
+++ src/shared/CardsList/Card/CommentForm/CommentForm.tsx
@@ -1,32 +1,20 @@
 import React, { ChangeEvent, FormEvent } from 'react';
 import * as styles from './commentForm.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateComment } from '../../../../store'; // Импортируйте экшен из commentSlice
 
-interface RootState {
-  commentText: string;
+type Props = {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  onSubmit: (e: FormEvent) => void;
 }
 
-export function CommentForm() {
-
-  const value = useSelector((state: RootState) => state.commentText);
-  const dispatch = useDispatch();
-
-  function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
-    dispatch(updateComment(e.target.value));
-  }
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    console.log('Comment submitted:', value);
-  }
+export function CommentForm({value, onChange, onSubmit}: Props) {
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <textarea
         className={styles.input}
         value={value}
-        onChange={handleChange}
+        onChange={onChange}
         placeholder="Write your comment here..."
       />
       <button type="submit" className={styles.button}>
---
+++ src/shared/CardsList/Card/CommentFormContainer/CommentFormContainer.tsx
@@ -0,0 +1,31 @@
+import React, { ChangeEvent, FormEvent } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateComment } from '../../../../store'; // Импортируйте экшен из commentSlice
+import { CommentForm } from '../CommentForm/CommentForm';
+
+interface RootState {
+  commentText: string;
+}
+
+export function CommentFormContainer() {
+
+  const value = useSelector((state: RootState) => state.commentText);
+  const dispatch = useDispatch();
+
+  function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    dispatch(updateComment(e.target.value));
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log('Comment submitted:', value);
+  }
+
+  return (
+    <CommentForm 
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+    />
+  );
+}
