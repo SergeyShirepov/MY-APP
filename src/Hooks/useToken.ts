74ddc5bd95cd636e089b03e23aed094819191c1e
SergeyShirepov <shirepov@internet.ru>
2025-02-01 01:25:41 +0300

    Асинхронный экшен

--- src/Hooks/useToken.ts
+++ src/Hooks/useToken.ts
@@ -1,12 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { RootState, store } from '../store';
+import { setToken } from '../store'; // Импортируем setToken
+import { ThunkAction } from 'redux-thunk';
+import { Action } from 'redux';
 
-export function useToken() {
-  // Инициализируем токен значением initialToken, если оно есть
-  const [token, setToken] = useState('');
+
+const timeout = (): ThunkAction<void, RootState, unknown, Action<string>> => (dispatch, getState) =>{
+  dispatch({ type: 'START'});
+  setTimeout(() => {
+  dispatch({ type: 'FINISH'});
+  }, 1500)
+}
+
+export function useToken(): [string | null] {
+  const [token, setTokenState] = useState<string | null>(null);
 
   useEffect(() => {
-    if (window.__token__) {
-      setToken(window.__token__);
+    const token = localStorage.getItem('token') || window.__token__;
+      store.dispatch(setToken(token)); // setToken возвращает объект действия
+      store.dispatch(timeout());
+    if (token) {
+      localStorage.setItem('token', token);
+      setTokenState(token); // Обновляем локальное состояние
     }
   }, []);
 
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,14 @@
-import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { configureStore, createSlice, PayloadAction, ThunkAction, Action } from "@reduxjs/toolkit";
+
 
 export type RootState = {
     commentText: string;
+    token: string | null;
   };
   
   const initialState: RootState = {
     commentText: 'Privet',
+    token: null,
   };
   
   export const commentSlice = createSlice({
@@ -15,14 +18,26 @@ export type RootState = {
       updateComment(state, action: PayloadAction<string>) {
         state.commentText = action.payload;
       },
+      setToken(state, action: PayloadAction<string | null>) {
+        state.token = action.payload;
+      },
     },
   });
   
   
   const { actions, reducer } = commentSlice;
-  export const { updateComment } = actions;
+  export const { updateComment, setToken  } = commentSlice.actions;
+
+
+  
+
   
   // Configure the Redux store
   export const store = configureStore({
     reducer,
+    
   });
+
+function dispatch(arg0: { type: string; }): ThunkAction<void, RootState, unknown, Action<string>> {
+  throw new Error("Function not implemented.");
+}
