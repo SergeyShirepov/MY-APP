4b68216d9cabdf133b4659f78bd155cad6ba62b6
ShirepovSergey <shirepov@internet.ru>
2025-02-13 23:06:22 +0300

    + react-router-dom для поста

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from './shared/Layout';
 import { Header } from './shared/Header';
 import { Content } from './shared/Content';
@@ -8,20 +8,46 @@ import { useToken } from './Hooks/useToken';
 import { tokenContext } from './shared/context/tokenContext';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Link, Routes, Router, Route } from 'react-router-dom';
+import { Post } from './shared/CardsList/Card/Post';
 
 // The main App component
 export function App() {
   const [token] = useToken();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
   
   return (
     <Provider store={store}>
       <tokenContext.Provider value={token}>
+        <BrowserRouter>
         <Layout>
           <Header />
           <Content>
             <CardsList />
+            <Routes>
+            <Route path="/posts/:id" element ={
+              
+            <Post card={{
+              id: '',
+              tittle: '',
+              cardPreview: '',
+              timePublished: '',
+              timeViewed: '',
+              avtor: '',
+              avatar: ''
+            }} />
+            }>
+            </Route>
+            </Routes>
           </Content>
         </Layout>
+        </BrowserRouter>
       </tokenContext.Provider>
     </Provider>
   );
--- src/shared/CardsList/Card/Tittle/Tittle.tsx
+++ src/shared/CardsList/Card/Tittle/Tittle.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as styles from './tittle.css';
-import { Post } from '../Post';
+import { Link } from 'react-router-dom';
 
 interface ICardProps {
     card: {
@@ -16,31 +16,22 @@ interface ICardProps {
 
 
 export function Tittle ({ card }: ICardProps) {
-    const [IsModalOpened, setIsModalOpened] = useState(false);
     let cardLength: number = card.tittle.length;
     let cardTittle;
+
     if (cardLength >= 62) {
         cardTittle = [card.tittle.slice(0, 62), `...`].join('');
     } else{
-        cardTittle = card.tittle.slice(0, 62);}
-
+        cardTittle = card.tittle.slice(0, 62);
+    }
 
     return (
         <h2 className={styles.tittle}>
-            <span className={styles.postlink} onClick={(e) => {
-                 setIsModalOpened(true);
-            }}>
+            <Link to="/posts/1" className={styles.postlink} >
                 {cardTittle}
-            </span>
+            </Link>
 
-            {IsModalOpened && (
-                <Post
-                    card={ card }
-                    onClose={() => {
-                        setIsModalOpened(false);
-                    }}
-                />
-            )}
+
 
 
         </h2>
