bd677ba0a51f1208694d93bea52e2dacbbec4053
ShirepovSergey <shirepov@internet.ru>
2025-03-13 00:33:08 +0300

    +

--- src/shared/CardsList/Card/KarmaCounter/KarmaCounter.tsx
+++ src/shared/CardsList/Card/KarmaCounter/KarmaCounter.tsx
@@ -10,13 +10,13 @@ const KarmaCounter = ({card}: ICardProps) => {
 
     return (
         <div className={styles.karmaCounter}>
-            <button className={styles.up} onClick={() => updateKarma(1)}>
+            <button className={styles.up} onClick={() => updateKarma(1)} disabled={isLoading}>
                 <svg width="19" height="10" viewBox="0 0 19 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.5 0L0 10H19L9.5 0Z" fill="#C4C4C4"/>
                 </svg>
             </button>
             <span className={styles.karmavalue}>{karmaValue}</span>
-            <button className={styles.down} onClick={() => updateKarma(-1)}>
+            <button className={styles.down} onClick={() => updateKarma(-1)} disabled={isLoading}>
                 <svg width="19" height="10" viewBox="0 0 19 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.5 0L0 10H19L9.5 0Z" fill="#C4C4C4"/>
                 </svg>
