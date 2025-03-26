7bd7b034ff81fc126fbccddf093e978b76894e40
ShirepovSergey <shirepov@internet.ru>
2025-03-27 00:00:01 +0300

    +Немного стилей)

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as styles from './layout.css';
-import { BrowserRouter, useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useToken } from '../../Hooks/useToken';
 import usePosts from '../../Hooks/usePosts';
 import { useSortedAndSearchPosts } from '../../Hooks/useSortedAndSearchPosts';
-import { Navi } from '../Navi/Navi';
 import { CardsList } from '../CardsList';
 import { Content } from '../Content';
 import { Header } from '../Header';
@@ -21,9 +20,6 @@ export function Layout() {
   const { posts = [], isLoading, error, hasMore, loadMorePosts } = usePosts(0, 6, sortBy);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const location = useLocation();
-
-  const isAccountPage = location.pathname === '/account';
  
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -67,14 +63,13 @@ export function Layout() {
 
 
   return (
-    <>
+    <div className={styles.container}>
       <Header onSortChange={handleSortChange} onSearchChange={setSearchBy} />
-      {isAccountPage && <Navi /> }
       <Content>
         <CardsList posts={sortedAndSearchPosts} />
         <div ref={observerTarget} style={{ height: '10px' }}></div>
         {isLoading && <div>Loading...</div>}
       </Content>
-    </>
+    </div>
   );
 }
